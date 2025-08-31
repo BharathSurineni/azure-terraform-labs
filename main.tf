35ade7490753ee5ce6a723ea0e8ce1809ec0f2fe
bharath surinenei <bharath.neni@gmail.com>
2025-08-31 13:32:09 +0530

    Add complete Terraform configuration with provider settings

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+  required_version = ">= 1.3.0"
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-provider-test-new"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "stsurinlab123" # must be unique globally
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
